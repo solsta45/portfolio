6403cf72454bf6e8f1416dd939b701d8e97f40db
Solveig <solsta99@gmail.com>
2023-06-12 17:48:27 +0200

    burger

--- menu.js
+++ menu.js
@@ -1,16 +1,19 @@
 const burger = document.querySelector(".burger");
 const nav = document.querySelector("nav");
-const menu = document.querySelector(".menu");
-const links = document.querySelectorAll(".menu li a");
+const menu = document.querySelector(".menu_02");
+const links = document.querySelectorAll(".menu_02 li a");
+const dropdownMenu = document.querySelector(".dropdown-menu");
 
 burger.addEventListener("click", () => {
   burger.classList.toggle("active");
   nav.classList.toggle("active");
+  dropdownMenu.classList.toggle("active");
 });
 
 links.forEach((link) => {
   link.addEventListener("click", () => {
     burger.classList.remove("active");
     nav.classList.remove("active");
+    dropdownMenu.classList.remove("active");
   });
 });
